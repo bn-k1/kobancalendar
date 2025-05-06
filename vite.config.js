b6862dd347527a0ce335d4d2136402ed0cda8296
bn-k1 <bn-k1@proton.me>
2025-05-06 20:44:16 +0900

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/kobancalendar/",
   plugins: [vue()],
   resolve: {
     alias: {
